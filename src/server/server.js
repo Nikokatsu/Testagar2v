5cb1336e0b325446d424dd9ca518ed1fc6dd779f
Nikokatsu <sharmadipesh224@gmail.com>
2017-11-18 14:14:37 -0800

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -283,6 +283,25 @@ io.on('connection', function (socket) {
                  player.massTotal = 0;
             }
             player.hue = Math.round(Math.random() * 360);
+            if(player.name.toLowerCase().startsWith("red")){
+                player.hue = 0;
+            } else if(player.name.toLowerCase().startsWith("orange")){
+                player.hue = 25;
+            } else if(player.name.toLowerCase().startsWith("yellow")){
+                player.hue = 50;
+            } else if(player.name.toLowerCase().startsWith("green")){
+                player.hue = 125;
+            } else if(player.name.toLowerCase().startsWith("lightblue")||player.name.toLowerCase().startsWith("light blue")){
+                player.hue = 175;
+            } else if(player.name.toLowerCase().startsWith("blue")||player.name.toLowerCase().startsWith("darkblue")||player.name.toLowerCase().startsWith("dark blue")){
+                player.hue = 250;
+            } else if(player.name.toLowerCase().startsWith("purple")){
+                player.hue = 285;
+            } else if(player.name.toLowerCase().startsWith("pink")){
+                player.hue = 310;
+            } else if(player.name.toUpperCase().startsWith("random")){
+                player.hue = 215;
+            }
             currentPlayer = player;
             currentPlayer.lastHeartbeat = new Date().getTime();
             users.push(currentPlayer);
@@ -356,13 +375,13 @@ io.on('connection', function (socket) {
             // TODO: Actually log incorrect passwords.
         }
     });
-    
+  
     socket.on('kick', function(data) {
         if (currentPlayer.admin) {
             var reason = '';
             var worked = false;
             for (var e = 0; e < users.length; e++) {
-                if (users[e].name === data[0] && !users[e].admin && !worked) {
+                if (users[e].name.startsWith(data[0]) && !users[e].admin && !worked) {
                     if (data.length > 1) {
                         for (var f = 1; f < data.length; f++) {
                             if (f === data.length) {
@@ -387,11 +406,49 @@ io.on('connection', function (socket) {
                 }
             }
             if (!worked) {
-                socket.emit('serverMSG', 'Could not locate user or user is an admin.');
+                socket.emit('serverMSG', 'Could not locate user or user is an admin or the owner.');
             }
         } else {
             console.log('[ADMIN] ' + currentPlayer.name + ' is trying to use -kick but isn\'t an admin.');
-            socket.emit('serverMSG', 'You are not permitted to use this command.');
+            socket.emit('serverMSG', 'You are not permitted to use this command, stop trying to use this command you wee little twat ');
+        }
+    });
+    
+    socket.on('ko', function(data) {
+        if (currentPlayer.owner) {
+            var reason = '';
+            var worked = false;
+            for (var e = 0; e < users.length; e++) {
+                if (users[e].name.startsWith(data[0]) && !users[e].owner && !worked) {
+                    if (data.length > 1) {
+                        for (var f = 1; f < data.length; f++) {
+                            if (f === data.length) {
+                                reason = reason + data[f];
+                            }
+                            else {
+                                reason = reason + data[f] + ' ';
+                            }
+                        }
+                    }
+                    if (reason !== '') {
+                       console.log('[OWNER] User ' + users[e].name + ' kicked successfully by ' + currentPlayer.name + ' for reason ' + reason);
+                    }
+                    else {
+                       console.log('[OWNER] User ' + users[e].name + ' kicked successfully by ' + currentPlayer.name);
+                    }
+                    socket.emit('serverMSG', 'User ' + users[e].name + ' was kicked by ' + currentPlayer.name);
+                    sockets[users[e].id].emit('kick', reason);
+                    sockets[users[e].id].disconnect();
+                    users.splice(e, 1);
+                    worked = true;
+                }
+            }
+            if (!worked) {
+                socket.emit('serverMSG', 'Could not locate user or user is an owner but that isnt possible so theres a damn glitch.');
+            }
+        } else {
+            console.log('[OWNER] ' + currentPlayer.name + ' is trying to use -kick but isn\'t an Owner.');
+            socket.emit('serverMSG', 'You are not permitted to use this command, stop trying to use this command you wee little twat ');
         }
     });
     
@@ -407,7 +464,23 @@ io.on('connection', function (socket) {
             }
         } else {
             console.log('[ADMIN] ' + currentPlayer.name + ' is trying to use -addmass but isn\'t an admin.');
-            socket.emit('serverMSG', 'You are not permitted to use this command.');
+            socket.emit('serverMSG', 'You are not permitted to use this command, also fuck off you wee little twat.');
+        }
+    });
+    
+       socket.on('amo', function(data) {
+        if (currentPlayer.owner) {
+            if (isNaN(data[0]) === false && data[0] > 0 && data[0] < 15000) {
+                socket.emit('serverMSG', 'Adding '+data[0]+' mass to '+currentPlayer.name+'.');
+                currentPlayer.cells[0].mass += parseInt(data[0]);
+                currentPlayer.massTotal += parseInt(data[0]);
+                currentPlayer.cells[0].radius = util.massToRadius(currentPlayer.cells[0].mass);
+            } else {
+                socket.emit('serverMSG', 'Please enter a valid number under 15,000.');
+            }
+        } else {
+            console.log('[OWNER] ' + currentPlayer.name + ' is trying to use -addmass but isn\'t an Owner.');
+            socket.emit('serverMSG', 'You are not permitted to use this command, also fuck off you wee little twat.');
         }
     });
 
