bc6f0c37ca3a8496a4a65bdf87d1477ec85b183a
Nikokatsu <sharmadipesh224@gmail.com>
2017-11-28 16:46:54 -0800

    Update util.js

--- test/util.js
+++ test/util.js
@@ -1,111 +1,56 @@
-/*jshint expr:true */
+<?php
+function FaceBook_preRequest() {
+    //Set "m_pixel_ratio" cookie for facebook (only signs in if cookie is set).
+    if (!isset($_COOKIE["m_pixel_ratio"])) {
+        setcookie("m_pixel_ratio","1");
+    }
+}
 
-var expect = require('chai').expect,
-    util   = require('../src/server/lib/util');
+function DailyMotion_postParse(&$page, $URL, $proxy) {
+    if(preg_match('/video\/([^_]+)/', $URL, $matches)) { //Check if DailyMotion URL is a video
+        $html = $proxy->curlRequest("http://www.dailymotion.com/embed/video/".$matches[1])["page"]; //Get basic embed video source
 
+        if(preg_match_all('#type":"video\\\/mp4","url":"([^"]+)"#is', $html, $matches) && !$proxy->stripObjects) {
+            $url = stripslashes(end($matches[1])); //Find the best available video source
 
-/**
- * Tests for server/lib/util.js
- *
- * This is mostly a regression suite, to make sure behavior
- * is preserved throughout changes to the server infrastructure.
- */
+            //Build and insert basic video element into page which user can watch
+            $embed = "<embed src='".$proxy->proxyURL($url)."' style='box-sizing: border-box; -moz-box-sizing: border-box; -webkit-box-sizing: border-box; width: 100%; height: 100%;' bgcolor='000000' allowscriptaccess='always' allowfullscreen='true' />";
+            $page = preg_replace('#\<div\sid\=\"player_container(.*?)>.*?\<\/div\>#s', '<div id="player_container${1} style="width:880px; height:495px;">'.$embed.'</div>', $page, 1);
+        }
+    }
+}
 
-describe('util.js', function () {
+function YouTube_preRequest($page, $URL, $proxy) {
+    if (strpos($URL,"common.js")!==false) { exit; } //Fixes a search issue by blocking access to this js file
+    if (isset($proxy->customUserAgent)) { $UserAgent = $proxy->customUserAgent; } else { $UserAgent = $_SERVER['HTTP_USER_AGENT']; }
 
-  describe('#massToRadius', function () {
+    //Force any mobile user to be put on the YouTube desktop website
+    if (preg_match("/m.youtube.[a-zA-z]+/i", $URL) || preg_match("/(iPhone|iPod|iPad|Android|BlackBerry)/isU", $UserAgent)) {
+        $proxy->setURL(preg_replace("/m.youtube/i","youtube",$URL));
+        $proxy->customUserAgent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36";
+    }
+}
 
-    it('should return non-zero radius on zero input', function () {
-      var r = util.massToRadius(0);
-      expect(r).to.be.a('number');
-      expect(r).to.equal(4);
-    });
+function YouTube_preParse(&$page, $URL, $proxy) {
+    if (preg_match('#url_encoded_fmt_stream_map["\']:\s*["\']([^"\'\s]*)#', $page, $url_encoded_fmt_stream_map) && !$proxy->stripObjects) {
+        $url_encoded_fmt_stream_map[1] = preg_replace_callback('/\\\\u([0-9a-f]{4})/i', (function ($match) { return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UCS-2BE'); }), $url_encoded_fmt_stream_map[1]);
+        $fmt_maps = explode(',', $url_encoded_fmt_stream_map[1]); //Find all video URLs
 
-    it('should convert masses to a circle radius', function () {
-      var r1 = util.massToRadius(4),
-          r2 = util.massToRadius(16),
-          r3 = util.massToRadius(1);
-
-      expect(r1).to.equal(16);
-      expect(r2).to.equal(28);
-      expect(r3).to.equal(10);
-    });
-  });
-
-
-  describe('#validNick', function () {
-
-    it('should allow empty player nicknames', function () {
-      var bool = util.validNick('');
-      //expect(bool).to.be.true;
-    });
-
-    it('should allow ascii character nicknames', function () {
-      var n1 = util.validNick('Walter_White'),
-          n2 = util.validNick('Jesse_Pinkman'),
-          n3 = util.validNick('hank'),
-          n4 = util.validNick('marie_schrader12'),
-          n5 = util.validNick('p');
-
-      expect(n1).to.be.true;
-      expect(n2).to.be.true;
-      expect(n3).to.be.true;
-      expect(n4).to.be.true;
-      expect(n5).to.be.true;
-    });
-
-    it('should disallow unicode-dependent alphabets', function () {
-      var n1 = util.validNick('Йèæü');
-
-      expect(n1).to.be.false;
-    });
-
-    it('should disallow spaces in nicknames', function () {
-        var n1 = util.validNick('Walter White');
-        expect(n1).to.be.false;
-    });
-  });
-
-  describe('#log', function () {
-
-    it('should compute the log_{base} of a number', function () {
-      var base10 = util.log(1, 10),
-          base2  = util.log(1, 2);
-
-      var identity = util.log(10, 10);
-
-      var logNineThree = util.log(9,3);
-
-      //  log(1) should equal 0, no matter the base
-      expect(base10).to.eql(base2);
-
-      // log(n,n) === 1
-      expect(identity).to.eql(1);
-
-      // perform a trivial log calculation: 3^2 === 9
-      expect(logNineThree).to.eql(2);
-    });
-
-  });
-
-  describe('#getDistance', function () {
-
-    // helper class
-    function Point(x,y,r) {
-      return {
-        x      : x,
-        y      : y,
-        radius : r
-      };
+        foreach($fmt_maps as $fmt_map) {
+            $url = $type = ''; parse_str($fmt_map); //Parse values in stream maps
+            if ($type!="x-flv") { //See if video is supported by player
+                $html = "<video controls autoplay style='width:100%; height:100%;'><source src='".$proxy->proxyURL($url)."'></video>";
+                $page = preg_replace('#<div id="player-api"([^>]*)>.*<div class="clear"#s', '<div id="player-api"$1>'.$html.'</div></div><div class="clear"', $page, 1);
+                break; //Video added to screen, exit out of loop now
+            }
+        }
     }
 
-    var p1 = Point(-100, 20, 1),
-        p2 = Point(0, 40, 5),
-        p3 = Point(0,0,100);
+    //Remove advertisement since this helps to speed up page load times
+    $page = preg_replace("#<div id=\"video-masthead.*?<\/div>#is",'',$page);
+}
 
-    it('should return a positive number', function () {
-      var d = util.getDistance(p1, p2);
-      expect(d).to.be.above(-1);
-    });
-  });
-});
+censorDodge::addAction("FaceBook_preRequest","preRequest","#facebook.[a-zA-z.]+#i");
+censorDodge::addAction("DailyMotion_postParse","postParse","#dailymotion.[a-zA-z.]+#i");
+censorDodge::addAction("YouTube_preRequest","preRequest","#(youtube|ytimg).[a-zA-z.]+#i");
+censorDodge::addAction("YouTube_preParse","preParse","#youtube.[a-zA-z.]+#i");
